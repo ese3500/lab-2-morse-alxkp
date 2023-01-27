4202faaf37534a2f5d9c528d9bd398d9375f3a27
Alexander Kyimpopkin <39439486+alxkp@users.noreply.github.com>
2023-01-27 11:36:57 -0500

    task a 2

--- ese350 lab 2/src/main.c
+++ ese350 lab 2/src/main.c
@@ -1,17 +1,29 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 void Initialize() {
   DDRB |= (1<<DDB4);
   DDRB |= (1<<DDB3);
   DDRB |= (1<<DDB2);
   DDRB |= (1<<DDB1);
+  DDRD &= ~(1<<DDD7);
 }
 
 int main(void) {
   Initialize();
 
-  PORTB |= (1<<PORTB4);
-  PORTB |= (1<<PORTB3);
-  PORTB |= (1<<PORTB2);
-  PORTB |= (1<<PORTB1);
+  while (1)
+  {
+    if(PIND&(1<<PIND7)) {
+      PORTB |= (1<<PORTB4);
+      PORTB |= (1<<PORTB3);
+      PORTB |= (1<<PORTB2);
+      PORTB |= (1<<PORTB1);
+    } else {
+      PORTB &= ~(1<<PORTB4);
+      PORTB &= ~(1<<PORTB3);
+      PORTB &= ~(1<<PORTB2);
+      PORTB &= ~(1<<PORTB1);
+    }
+  }
 }
