400be46c7d00048e96000d71ecf7d97a14f317dc
Alexander Kyimpopkin <39439486+alxkp@users.noreply.github.com>
2023-01-27 11:49:34 -0500

    task a 3

--- ese350 lab 2/src/main.c
+++ ese350 lab 2/src/main.c
@@ -12,18 +12,42 @@ void Initialize() {
 int main(void) {
   Initialize();
 
+  int counter = 0;
+
   while (1)
   {
-    if(PIND&(1<<PIND7)) {
-      PORTB |= (1<<PORTB4);
-      PORTB |= (1<<PORTB3);
-      PORTB |= (1<<PORTB2);
-      PORTB |= (1<<PORTB1);
-    } else {
-      PORTB &= ~(1<<PORTB4);
-      PORTB &= ~(1<<PORTB3);
-      PORTB &= ~(1<<PORTB2);
-      PORTB &= ~(1<<PORTB1);
+    if (PIND & (1 << PIND7)) {
+      if (counter == 4) {
+        counter = 0;
+      } else {
+        counter++;
+      }
+    }
+    
+    switch (counter)
+    {
+    case 0:
+      PORTB &= ~(0x1C);
+      _delay_ms(200);
+      PORTB |= (1 << PORTB1);
+      break;
+    case 1:
+      PORTB &= ~(0x1A);
+      _delay_ms(200);
+      PORTB |= (1 << PORTB2);
+      break;
+    case 2:
+      PORTB &= ~(0x16);
+      _delay_ms(200);
+      PORTB |= (1 << PORTB3);
+      break;
+    case 3:
+      PORTB &= ~(0xE);
+      _delay_ms(200);
+      PORTB |= (1 << PORTB4);
+      break;
+    default:
+      break;
     }
   }
 }
